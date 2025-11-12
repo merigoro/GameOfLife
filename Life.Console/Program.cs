dbd45ba47a4eb6ffb672c3810e60a48d6efe4f2d
Roberto Merigo <roberto.merigo@camngo.it>
2025-11-12 19:37:46 +0100

    Clean code

--- Life.Console/Program.cs
+++ Life.Console/Program.cs
@@ -6,12 +6,12 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 int rows = args.Length > 0 && int.TryParse(args[0], out var r) ? r : 25;
 int cols = args.Length > 1 && int.TryParse(args[1], out var c) ? c : 50;
-double p = args.Length > 2 && double.TryParse(args[2], out var prob) ? prob : 0.25;
+double probability = args.Length > 2 && double.TryParse(args[2], out var prob) ? prob : 0.25;
 int speedMs = args.Length > 3 && int.TryParse(args[3], out var ms) ? ms : 100;
 int? seed = args.Length > 4 && int.TryParse(args[4], out var s) ? s : null;
 
 var size = new GameGrid(rows, cols);
-var grid = GameService.Random(size, p, seed);
+var grid = GameService.Random(size, probability, seed);
 
 bool running = true;
 Console.CancelKeyPress += (_, e) => { e.Cancel = true; running = false; };
